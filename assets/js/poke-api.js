57916d996df27c7c365d2f39707e391d93e10ed2
Douglas-Rodrigues1988 <dlorodriguesweb3@gmail.com>
2024-10-09 11:07:39 -0300

    Desafio
    Concluido

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,4 +1,3 @@
-
 const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
@@ -32,4 +31,110 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
+}
+
+// Conversão de detalhes dos pokemons
+pokeApi.convertDetails = (pokemon) =>{
+    const imagePokemon = pokemon.sprites.front_default
+    const pokemonDetail = new PokemonDetails(
+        pokemon.name,
+            pokemon.order,
+                pokemon.height,
+                    pokemon.weight,
+                        pokemon.stats,
+                            pokemon.types,
+                                imagePokemon)
+    
+    return pokemonDetail
+}
+
+// Detalhes de visualização dos pokemons
+pokeApi.viewDetails = (pokemon)=>{
+    const typeColor = pokemon.types[0].type.name
+    
+    pokemon.weight = pokemon.weight / 10 
+    pokemon.height = pokemon.height / 10
+
+
+    return `
+        <section class="modalPoke ${typeColor}">
+            <div class="header">
+                <span class="number">#${pokemon.order}</span>
+                <h1 class="namePoke">${pokemon.name}</h1>
+            </div>
+
+            <div class="image">
+                <img class="imagePoke" src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+
+            <div class="deitalsAndPower">
+                <div class="detailsBox">
+                    <h2 class="detailsTitle">
+                        Details
+                    </h2>
+                    
+                    <div class="detailsWidth">
+                        <span class="detailText ${typeColor}" id="width">
+                            ${pokemon.height} M
+                        </span>
+                        <span class="detailText ${typeColor}" id="height">
+                            ${pokemon.weight} KG
+                        </span>
+                    </div>
+                </div>
+
+                <div class="types">
+                    <h2>
+                        Powers Pokemon
+                    </h2>
+                    <ul class="typesList">
+                        ${viewPower(pokemon.types)}
+                    </ul>
+                </div>
+            </div>
+
+
+            <div class="statusPoke">
+                <h1 class="namePoke">
+                    Status ${pokemon.name}
+                </h1>
+                ${viewStatus(pokemon.status)}
+            </div>
+            
+
+            <div class="buttonRemove">
+                <button onclick="removePokeDetails()" class="btnRemove">
+                    Sair
+                </button>
+            </div>
+        </section>
+    `
+}
+
+let removePokeDetails = ()=>{
+    const modalPoke = document.querySelector('.modalPoke')
+    modalPoke.remove()
+}
+
+let viewStatus = (statusPoke) =>{
+    return statusPoke.map((statusI) =>{
+        const valueStatus = statusI.base_stat
+        const nameStatus = statusI.stat.name
+        
+        return `
+        <div class="statusBox">
+            <h3 class="titleStatus">${nameStatus}</h3>
+            <div class="box ${nameStatus}">${valueStatus}%</div>
+        </div>`
+    }).join('')
+}
+
+let viewPower = (typePokemon) =>{
+    return typePokemon.map((typePoke) => {
+        const typeName = typePoke.type.name;
+        return `
+        <li class="typeItem ${typeName}">
+            ${typeName}
+        </li>`
+    }).join('')
 }
